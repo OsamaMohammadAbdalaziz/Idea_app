5c8169406862f6c7e3d13c020e72be8af9edab0e
7iilko <arabradui170@gmail.com>
2023-04-18 10:48:26 +0200

    create body - use Image widget (NetworkImage)

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -11,8 +11,12 @@ class Home extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Color.fromRGBO(0, 145, 255, 1),
         appBar: AppBar(
-          title: Text("Fikra"),
+          title: Text("فكرة"),
           backgroundColor: Color.fromARGB(255, 0, 0, 255),
+        ),
+        body: Image(
+          image: NetworkImage(
+              "https://png.pngtree.com/element_our/png_detail/20190103/ideainsightkeylamplightbulb-line-icon-png_307494.jpg"),
         ),
       ),
     );
