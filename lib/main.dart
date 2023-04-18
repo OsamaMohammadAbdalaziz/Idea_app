9df4456ef283e17270e13188a919a5293e184a13
7iilko <arabradui170@gmail.com>
2023-04-18 10:29:40 +0200

    use statelesswidget-create folder(Pages)-create file(Home)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:idae_app/Pages/Home.dart';
 
 //The main function is the starting point for all our Flutter Apps
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
-      ),
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
 }
